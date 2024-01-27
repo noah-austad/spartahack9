afd523f196a36f6ea68056702a6a1a65c7e15570
noah-austad <122561843+noah-austad@users.noreply.github.com>
2024-01-27 15:32:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-app.run(port=5000)
+@app.route('/launch')
+def launch():
+    return render_template('launch.html')
+
+if __name__ == '__main__':
+    app.run(port=5000)
