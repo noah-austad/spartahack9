ad7e9eb21a90fa8edcb318ab1941ff11bf8d4fb2
nmpartridge13 <129319685+nmpartridge13@users.noreply.github.com>
2024-01-27 21:39:01 -0500

    Update options_data.py

--- Spartahack 9/options_data.py
+++ Spartahack 9/options_data.py
@@ -1,6 +1,8 @@
-#API Key:
+#API Keys:
 #G6H0CSGDHM0L3VGB
+#CI7ESVAE610SFJNF
 import requests
+import json
 #Option Trading Classes
 class Option:
     #constructor method
@@ -17,14 +19,20 @@ class stock:
         self.ticker=ticker
         self.price_history=price
     def update_prices(self):
-        url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+self.ticker+'&apikey=G6H0CSGDHM0L3VGB'
+        url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+self.ticker+'&apikey=CI7ESVAE610SFJNF'
         r = requests.get(url)
         data = r.json()
         close_data = [entry['4. close'] for entry in data['Time Series (Daily)'].values()]
         self.price_history=close_data
 
 def trending_req():
-    url = 'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=G6H0CSGDHM0L3VGB'
-    r = requests.get(url)
-    data = r.json()
-    return(data)
+    # url = 'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=CI7ESVAE610SFJNF'
+    # r = requests.get(url)
+    # data = r.json()
+    # data=data["most_actively_traded"]
+    # return(data)
+    f=open('C:\\Users\\noahp\\Downloads\\info.json')
+    data=json.load(f)
+    active=json.dumps(data['most_actively_traded'])
+
+    return(active)
