e917c0b2f845e6b6c7710f97c00206cf4891467e
nmpartridge13 <129319685+nmpartridge13@users.noreply.github.com>
2024-01-27 19:51:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from options_data import stock, trending_req
+from flask import Flask, render_template, request
+from json2table import convert
 
 app = Flask(__name__)
 
@@ -9,6 +11,22 @@ def home():
 @app.route('/launch')
 def launch():
     return render_template('launch.html')
+
+@app.route('/trending')
+def trending():
+    data=trending_req()
+    build_direction="LEFT_TO_RIGHT"
+    table_attributes={"style" : "width:100%"}
+    table=convert(data,build_direction=build_direction,table_attributes=table_attributes)
+    return render_template('trending.html',table=table)
+
+@app.route('/strategies', methods=['POST'])
+def strategies():
+    ticker_str = request.form['user_input']
+    asset=stock(ticker_str)
+    asset.update_prices()
+    return render_template('strategies.html', recent=asset.price_history, ticker=asset.ticker)
+
 
 if __name__ == '__main__':
     app.run(port=5000)
